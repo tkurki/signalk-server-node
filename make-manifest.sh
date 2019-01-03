b302d839d66baafc4e5d4e017235bed2265b7ab0
Teppo Kurki <teppo.kurki@iki.fi>
2019-01-03 18:47:09 +0200

    iterate

--- make-manifest.sh
+++ make-manifest.sh
@@ -1,13 +1,13 @@
 cat >docker-manifest.yml <<EOF
-image: 'tkurki/signalk-server:$BRANCH'
+image: 'tkurki/signalk-server:$TRAVIS_BRANCH'
 manifests:
   -
-    image: 'tkurki/signalk-server:linux-amd64-$BRANCH'
+    image: 'tkurki/signalk-server:linux-amd64-$TRAVIS_BRANCH'
     platform:
       architecture: amd64
       os: linux
   -
-    image: 'tkurki/signalk-server:linux-arm32v7-$BRANCH'
+    image: 'tkurki/signalk-server:linux-arm32v7-$TRAVIS_BRANCH'
     platform:
       architecture: arm
       variant: v7
